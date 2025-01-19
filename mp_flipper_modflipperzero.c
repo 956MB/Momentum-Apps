8dced0c572572c6f006d6f9479931de8e0a74833
Oliver Fabel <28701799+ofabel@users.noreply.github.com>
2025-01-19 05:59:34 +0100

    library update

--- mp_flipper_modflipperzero.c
+++ mp_flipper_modflipperzero.c
@@ -651,7 +651,7 @@ void flipperzero_module_attr(mp_obj_t self_in, qstr attr, mp_obj_t* dest) {
             } else {
                 float exponent = (octave * 12.0 + note - 57.0) / 12.0;
                 // dest[0] = mp_obj_new_float(440.0 * pow(2.0, (octave * 12.0 + note - 57.0) / 12.0));
-                dest[0] = mp_obj_new_float(440.0f * pow(2.0f, exponent));
+                dest[0] = mp_obj_new_float(440.0f * pow(2.0f, -4.75f));
             }
 
             return;
