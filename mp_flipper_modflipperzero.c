ee68d9bd5635a36fce0975dcf8fcf6919a74a6e6
Oliver Fabel <28701799+ofabel@users.noreply.github.com>
2025-01-19 03:59:31 +0100

    library update

--- mp_flipper_modflipperzero.c
+++ mp_flipper_modflipperzero.c
@@ -754,6 +754,8 @@ void flipperzero_module_attr(mp_obj_t self_in, qstr attr, mp_obj_t* dest) {
 
     if(strstr(attribute, "SPEAKER_NOTE_") == attribute[0]) {
         dest[0] = mp_obj_new_int(42);
+
+        return;
     } else {
         dest[0] = MP_OBJ_NULL;
     }
